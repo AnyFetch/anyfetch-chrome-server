a89280977e04f7d2cb6284428c460d7793282d2b
antonhansel <antoninribeaud@gmail.com>
2015-02-23 13:41:18 +0100

    Fix bad config

--- config/configuration.js
+++ config/configuration.js
@@ -17,8 +17,8 @@ dotenv.load();
 var nodeEnv = process.env.NODE_ENV || "development";
 var port = process.env.PORT || 8000;
 
-var managerUrl = process.env.MANAGER_URL || 'https://manager-staging.anyfetch.com';
-var apiUrl = process.env.API_URL || 'https://api-staging.anyfetch.com';
+var managerUrl = process.env.MANAGER_URL || 'https://manager.anyfetch.com';
+var apiUrl = process.env.API_URL || 'https://api.anyfetch.com';
 var apiId =  process.env.ANYFETCH_API_ID;
 var apiSecret = process.env.ANYFETCH_API_SECRET;
 
@@ -35,9 +35,9 @@ Anyfetch.setManagerUrl(managerUrl);
 // Third party services
 var services = {};
 services.opbeat = {
-  organization_id: process.env.OPBEAT_ORGANIZATION_ID,
+  organization_id: process.env.OPBEAT_ORG_ID,
   app_id: process.env.OPBEAT_APP_ID,
-  secret_token: process.env.OPBEAT_SECRET_TOKEN,
+  secret_token: process.env.OPBEAT_TOKEN,
   silent: true,
 };
 
@@ -46,13 +46,8 @@ module.exports = {
   env: nodeEnv,
   port: port,
 
-  // STAGING
-  // appId: "5463ba1e70e0ff7e2aa1814c",
-  // appSecret: "7b705a1ba19cd82dc9e46b566501f029a14d25c9481864d25a27d1890e28dabb",
-
-  // My test app
-  appId: "54dcdf1a88df99f45040b86f",
-  appSecret: "6b939d1d6f4fce4e866283ca398d438a9a54e5af470d74577d989e71b98464b9",
+  appId: apiId,
+  appSecret: apiSecret,
 
   serverUrl: process.env.SERVER_URL || 'http://localhost:' + port,
   managerUrl: managerUrl,
